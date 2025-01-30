2cd6c46384265cb0fce22601174b42cc8eab00d3
Pavel <hivikipof@gmail.com>
2025-01-30 21:42:17 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0F172A", // Azul oscuro
+        gold: "#CDAA6D",   // Dorado elegante
+        accent: "#1E293B", // Negro sobrio
+      },
+    },
+  },
+  plugins: [],
+};
