9bb76585234ddae3592e5de2454249c370c2aedc
Pavel <hivikipof@gmail.com>
2025-02-09 13:50:49 +0000

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import homeImage from "/images/home.webp";
+import logoImage from "/images/logo_jay.png";
 
 function Home({ onOpenWizard }) {
   return (
@@ -7,7 +8,7 @@ function Home({ onOpenWizard }) {
       id="home"
       className="relative w-full h-screen flex flex-col items-center justify-center bg-black text-white overflow-hidden"
       style={{
-        backgroundImage: `url(./images/home.webp)`,
+        backgroundImage: `url(${homeImage})`,
         backgroundSize: "cover",
         backgroundPosition: "right center",
         backgroundRepeat: "no-repeat",
@@ -18,38 +19,47 @@ function Home({ onOpenWizard }) {
 
       {/* Logo en la parte superior */}
       <div className="absolute top-8 left-1/2 transform -translate-x-1/2">
-        <img src="./images/logo_jay.png" alt="Logo" className="h-40" />
+        <img
+          src={logoImage}
+          alt="Logo de la empresa de transporte VIP"
+          className="h-40"
+        />
       </div>
 
       {/* Contenido principal */}
       <div className="relative z-10 text-left px-6 max-w-3xl self-start mr-6 lg:pt-40">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-          El transporte VIP que figuras de alto nivel eligen.
+         TRANSPORTE VIP
         </h1>
         <p className="text-sm sm:text-lg md:text-xl mb-6">
-          Especialistas en Traslados Ejecutivos, Niños Especiales, Adultos
-          Mayores y Eventos Especiales.
+        El eligido por las figuras de alto nivel.
         </p>
         <button
           onClick={onOpenWizard}
-          className="bg-gold bg-opacity-80 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-yellow-600 transition"
+          className="bg-gold bg-opacity-80 text-white px-6 py-3 rounded-full text-base font-semibold hover:bg-yellow-600 transition whitespace-normal text-center leading-tight w-30"
         >
-          Reserva Ahora <i class="ri-arrow-right-line"></i>
+          Reserva Ahora <i className="ri-arrow-right-line"></i>
         </button>
       </div>
 
       {/* Estadísticas en la parte inferior */}
       <div className="relative z-10 bottom-4 w-full flex justify-center gap-2 text-center text-lg text-xl font-semibold pt-40 lg:pt-0 md:pt-0 mt-30 lg:mt-0 md:mt-0 ">
         <div>
-          <span className="block text-2xl sm:text-3xl font-bold text-gold">10K+</span>
+          <span className="block text-2xl sm:text-3xl font-bold text-gold">
+            10K+
+          </span>
           <span className="text-white">Clientes satisfechos</span>
         </div>
         <div>
-          <span className="block text-2xl sm:text-3xl font-bold text-gold">20K+</span>
+          <span className="block text-2xl sm:text-3xl font-bold text-gold">
+            20+
+          </span>
           <span className="text-white">Años de experiencia</span>
         </div>
         <div>
-          <span className="block text-2xl sm:text-3xl font-bold text-gold">100</span>
+          <span className="block text-2xl sm:text-3xl font-bold text-gold">
+            100
+          </span>
           <span className="text-white">Conductores profesionales</span>
         </div>
       </div>
