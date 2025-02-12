02589856256606f3222d3d904dab6c2c8c31115b
Pavel <hivikipof@gmail.com>
2025-02-12 13:19:12 +0000

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,43 +6,44 @@ function Home({ onOpenWizard }) {
   return (
     <section
       id="home"
-      className="relative w-full h-screen flex flex-col items-center justify-center bg-black text-girs overflow-hidden"
+      className="relative w-full h-[955px] flex flex-col items-center justify-center bg-black text-girs overflow-hidden"
       style={{
         backgroundImage: `url(${homeImage})`,
         backgroundSize: "cover",
         backgroundPosition: "right center",
         backgroundRepeat: "no-repeat",
-        opacity:0.8
+        opacity: 0.8,
       }}
     >
       {/* Overlay oscuro para mejorar contraste */}
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
       {/* Logo en la parte superior */}
-      <div className="absolute top-8 left-1/2 transform -translate-x-1/2">
+      <div className="absolute top-[87px] left-1/2 transform -translate-x-1/2 w-[174px] h-[95.20879364013672px]">
         <img
           src={logoImage}
           alt="Logo de la empresa de transporte VIP"
-          className="h-40"
+          className="w-full h-full object-cover"
         />
       </div>
 
       {/* Contenido principal */}
-      <div className="relative z-10 text-left px-6 max-w-3xl self-start mr-6 lg:pt-40">
-        <h1 className="font-bebas  text-4xl md:text-5xl lg:text-6xl font-bebas mb-4 text-gris ">
-        Luxury Transport
+      <div className="absolute z-10 text-center px-6 max-w-[404px] w-[404px] h-[120px] top-[180px] left-[18px]">
+        <h1 className="font-bebas font-bold text-[39px]  tracking-normal ">
+          Luxury Transport
         </h1>
 
-        <p className="font-montserrat text-sm sm:text-lg md:text-xl mb-6">
-        An exclusive, refined, and comfortable experience designed to elevate all your travels
-        </p>
-        <button
-          onClick={onOpenWizard}
-          className="font-bebas  bg-gold bg-opacity-80 text-gris px-6 py-3 rounded-full text-base font-semibebas hover:bg-yellow-600 transition whitespace-normal text-center leading-tight w-30 "
-        >
-          Reserva Ahora <i className="ri-arrow-right-line"></i>
-        </button>
+        <span className="font-montserrat text-[19px]  tracking-normal block mb-6">
+          An exclusive, refined, and comfortable experience designed to elevate
+          all your travels
+        </span>
       </div>
+      <button
+        onClick={onOpenWizard}
+        className="absolute w-[146px] h-[55px] top-[393px] left-[20px] flex items-center justify-center gap-2.5 bg-gold text-white font-bold rounded-full shadow-md hover:bg-blue-600 transition duration-300"
+      >
+        Reserva Ahora <i className="ri-arrow-right-line"></i>
+      </button>
 
       {/* Estadísticas en la parte inferior */}
       <div className="relative z-10 bottom-4 w-full flex justify-center gap-2 text-center text-lg text-xl font-semibebas text-opacity-70 pt-40 lg:pt-0  mt-40 lg:mt-0  ">
@@ -50,21 +51,34 @@ function Home({ onOpenWizard }) {
           <span className="block text-2xl text-3xl font-bebas text-gold">
             10K+
           </span>
-          <span className="font-montserrat text-gris text-lg leading-7 text-center">Clientes satisfechos</span>
+          <span className="font-montserrat text-gris text-lg leading-7 text-center">
+            Clientes satisfechos
+          </span>
         </div>
         <div>
           <span className="block text-2xl text-3xl font-bebas text-gold">
             20+
           </span>
-          <span className="font-montserrat text-gris text-lg leading-7 text-center">Años de experiencia</span>
+          <span className="font-montserrat text-gris text-lg leading-7 text-center">
+            Años de experiencia
+          </span>
         </div>
         <div>
           <span className="block text-2xl text-3xl font-bebas text-gold">
             100
           </span>
-          <span className="font-montserrat text-gris text-lg leading-7 text-center">Conductores profesionales</span>
+          <span className="font-montserrat text-gris text-lg leading-7 text-center">
+            Conductores profesionales
+          </span>
         </div>
       </div>
+      <div className="absolute w-[55px] h-[61px] top-[865px] left-[193px] flex flex-col items-center justify-center text-center text-white z-10">
+        <span className="text-xs font-bold">SCROLL</span>
+        <i className="ri-arrow-down-s-line animate-bounce mt-1"></i>
+      </div>
+
+      {/* Capa de difuminación */}
+      <div className="absolute bottom-[0px] left-0 w-full h-40 bg-gradient-to-t from-black to-transparent pointer-events-none z-0"></div>
     </section>
   );
 }
